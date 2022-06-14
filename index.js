ea84537db7ece168a4d8ce4a8ab389c8fead1aab
Eliz39 <79478651+Eliz39@users.noreply.github.com>
2022-06-14 14:32:29 +0200

    feat: add solutions

---
+++ index.js
@@ -0,0 +1,24 @@
+// task-1
+const input11 = prompt('Задание 1. Введите число 1');
+const input12 = prompt('Задание 1. Введите число 2');
+
+if(isNaN(Number(input11)) || isNaN(Number(input12))) {
+  console.log('Некорректный ввод!');
+} else {
+  console.log(Number(input11).toString(Number(input12)))
+}
+
+// task-1
+const input21 = prompt('Задание 2. Введите число 1');
+
+if(isNaN(Number(input11))) {
+  console.log('Некорректный ввод!');
+} else {
+  const input22 = prompt('Задание 2. Введите число 2');
+  if(isNaN(Number(input11))) {
+      console.log('Некорректный ввод!');
+  } else {
+      console.log(+input21 + +input22, input21 / input22);
+  }
+}
+

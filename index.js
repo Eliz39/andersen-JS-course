510656e460d285a5873f90c3e1b1a2dd9fb8582e
Eliz39 <liza.nikitina9898@gmail.com>
2022-06-28 13:36:56 +0200

    feat: add 2nd solution

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// task 1
+
 function concatStrings(str1, sep1) {
   let curStr, separator = '';
 
@@ -24,5 +26,45 @@ function concatStrings(str1, sep1) {
     }
 
     return curStr;
+  }
+}
+
+// task 2
+
+class Calculator {
+  constructor(num1, num2) {
+    if (!num1 || !num2 || typeof num1 !== 'number' ||
+    typeof num2 !== 'number' || !isFinite(num1) || !isFinite(num2)) {
+      throw new Error('');
+    }
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  set setX(num) {
+    if(!num || typeof num !== 'number' || !isFinite(num)) {
+      throw new Error('');
+    }
+    this.num1 = num;
+  }
+  set setY(num) {
+    if(!num || typeof num !== 'number' ) {
+      throw new Error('');
+    }
+    this.num2 = num;
+  }
+  logSum() {
+    console.log(this.num1 + this.num2);
+  }
+  logMul() {
+    console.log(this.num1 * this.num2);
+  }
+  logSub() {
+    console.log(this.num1 - this.num2);
+  }
+  logDiv() {
+    if(this.num2 === 0) {
+      throw new Error('');
+    }
+    console.log(this.num1 / this.num2);
   }
 }

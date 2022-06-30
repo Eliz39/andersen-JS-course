0ec7d3bc74756dd4cd7fa8170ecae3ee6737eedd
Eliz39 <liza.nikitina9898@gmail.com>
2022-06-30 22:57:50 +0200

    feat: class stack

---
+++ stack.js
@@ -0,0 +1,19 @@
+class Stack {
+    stack = [];
+
+    push(element) {
+     this.stack.push(element);
+    }
+
+    pop() {
+      return this.stack.pop();
+    }
+
+    peek() {
+      return this.stack[this.stack.length - 1];
+    }
+  }
+
+  module.exports = {
+    Stack
+  };
